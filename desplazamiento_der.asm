a735f838f47f0ca4ef60003e94a7f58a92f3e426
Axelrpg <axelrpghd@gmail.com>
2023-02-27 11:58:08 -0600

    desplazamiento_der

--- desplazamiento_der.asm
+++ desplazamiento_der.asm
@@ -1,3 +1,7 @@
+section .data
+    ln db 10, 13
+    lonln equ 2
+
 section .bss
     resultado resb 1
 
@@ -5,7 +9,7 @@ section .text
     global _start
 
 _start:
-    mov ebx, 2
+    mov ebx, 128
     shr ebx, 1
 
     add ebx, 48
@@ -15,6 +19,12 @@ _start:
     mov ebx, 1
     mov ecx, resultado
     mov edx, 1
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, ln
+    mov edx, lonln
     int 0x80
 
     mov eax, 1
